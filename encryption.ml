c7485f1d988799a6640a5c9899ff17dff626531b
Tushar Khan <tak62@cornell.edu>
2019-11-20 13:31:48 -0500

    encryption stuff

--- encryption.ml
+++ encryption.ml
@@ -1,5 +1,7 @@
 
-(** RSA ENCRYPTION
+(** @author Tushar Khan (tak62) *)
+
+(* RSA ENCRYPTION
  * 
  * VARIABLES
  * p, q : distinct primes
@@ -27,20 +29,28 @@
  * - implement [generate_inverse] using Extended Euclidean algorithm
 *)
 
+(** [primes1] is a list of one million primes in the interval 
+    [961748941, 982451653], which spans exactly 1000000 primes. *)
+let primes1 = 
+  let filename = "primes1.txt" in print_endline ("Parsing "^filename^" ...");
+  let channel = open_in filename in
+
+  (** [parse_primes1 acc] is a list of the numbers from 'primes1.txt'. *)
+  let rec parse_primes1 acc =
+    match input_line channel |> String.trim with
+    | exception (End_of_file) -> acc
+    | str -> parse_primes1 ((parse_primes1_line str)@acc)
+  and parse_primes1_line line =
+    line |> String.split_on_char ' ' |> 
+    List.filter (fun s -> String.trim s <> "") |> 
+    List.map (fun s -> s |> String.trim |> int_of_string)
+  in
+  parse_primes1 []
+
 (** [generate_prime] is a prime number. *)
 let generate_prime () =
-  let small_primes = 
-    [3;5;7;11;13;17;19;23;29;31;37;41;43;47;53;59;61;67;71;73;79;83;89;97
-    ;101;103;107;109;113;127;131;137;139;149;151;157;163;167;173;179
-    ;181;191;193;197;199;211;223;227;229;233;239;241;251;257;263;269
-    ;271;277;281;283;293;307;311;313;317;331;337;347;349;353;359;367
-    ;373;379;383;389;397;401;409;419;421;431;433;439;443;449;457;461
-    ;463;467;479;487;491;499;503;509;521;523;541;547;557;563;569;571
-    ;577;587;593;599;601;607;613;617;619;631;641;643;647;653;659;661
-    ;673;677;683;691;701;709;719;727;733;739;743;751;757;761;769;773
-    ;787;797;809;811;821;823;827;829;839;853;857;859;863;877;881;883
-    ;887;907;911;919;929;937;941;947;953;967;971;977;983;991;997] in
-  small_primes |> List.length |> Random.int |> List.nth small_primes
+  let primes = primes1 in
+  primes |> List.length |> Random.int |> List.nth primes
 
 (** [gcd x y] is the greatest-common divisor of [x] and [y], computed using
     the Euclidean Division algorithm. *)
@@ -55,14 +65,23 @@ let generate_unit m =
     if gcd m i = 1 then i else find_unit m (i+1)
   in find_unit m 2
 
-(** [generate_inverse n m] is the inverse of [n] in the modulus [m], computed
+(** [generate_inverse n m] is the inverse of [n] in the modulo [m], computed
     using the Extended Euclidean algorithm. *)
 let generate_inverse n m =
-  let rec brute_force i = 
-    if (n * i) mod m = 1 then i else brute_force (i+1)
-  in brute_force 1
+  let rec extended_euclid r r' t t' : int =
+    print_endline ((string_of_int r)^" "^(string_of_int r')^" "^(string_of_int t)^" "^(string_of_int t'));
+    if r' = 0 then
+      if r > 1 then failwith ((string_of_int n)^" is non-invertible") else
+      if t < 0 then t + m else t
+    else let q = r / r' in extended_euclid r' (r-q*r') t' ((t-q*t') mod m)
+  in extended_euclid m n 0 1
+(*
+let rec brute_force i = 
+  if (n * i) mod m = 1 then i else brute_force (i+1)
+in brute_force 1
+*)
 
-(** [i_exp a b] is [a] to the power of [b] where [a] and [b] are integers. *)
+(** [i_exp a b] is a^b where [a] and [b] are integers. *)
 let i_exp a b = int_of_float ((float_of_int a) ** (float_of_int b))
 
 (** [mod_pow x m k] is (x mod m)^k, computed using the fast 
@@ -79,10 +98,14 @@ let mod_pow x m k =
     if d = 0 then acc else to_binary (d lsr 1) ((d land 1) :: acc)
   in
 
+  ignore (to_binary k [] |> List.map (fun x -> print_endline (string_of_int x)));
+  print_endline "---";
+  ignore (modular_exponents k [] |> List.map (fun x -> print_endline (string_of_int x)));
+
   let rec compute acc = function
     | (1, v)::y -> compute ((acc * v) mod m) y
-    | (0, v)::y -> compute acc y
-    | _ -> acc
+    | (0, _)::y -> compute acc y
+    | _ -> if acc < 0 then acc + m else acc
   in compute 1 (List.combine (to_binary k []) (modular_exponents k []))
 
 (** [generate_keys ()] is [(m, k, ki)] where [m, k] is the public key
@@ -94,7 +117,7 @@ let generate_keys () =
   let t = (p-1) * (q-1) in
   let k = generate_unit t in
   let ki = generate_inverse k t in
-  (m, k, ki)
+  (m, k, ki, p, q)
 
 (** [encrypt m k p] is an RSA-encrypted ciphertext of [p] 
     using the public key [m, k]. *)
@@ -105,4 +128,3 @@ let encrypt m k p =
     using the private key [ki] if [c] was computed using the modulus [m]. *)
 let decrypt m ki c =
   mod_pow c m ki
-
