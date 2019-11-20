7b8eb47c0aa4006a509a4c45a4a8963c0c508ad3
Aidan Chalnick <ac2597@cornell.edu>
2019-11-19 20:01:59 -0500

    added make aidan and some other stuff

--- aidanswackyocamladventure.ml
+++ aidanswackyocamladventure.ml
@@ -1,4 +1,11 @@
-let inet = Unix.PF_UNIX
+let inet = Unix.PF_INET
 let sost = Unix.SOCK_STREAM
 
 let sock_one = Unix.socket inet sost 0
+
+let n1 = Unix.gethostname ()
+let byname = Unix.gethostbyname n1
+let my_addr = byname.Unix.h_addr_list.(0)
+
+
+
