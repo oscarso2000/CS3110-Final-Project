d6a63f8147d8b3a01385af73f8c0dc61c24161bd
Tushar Khan <tak62@cornell.edu>
2019-12-10 23:32:54 -0500

    greedy algo

--- checkersAI.ml
+++ checkersAI.ml
@@ -55,8 +55,61 @@ let valid_moves t : move list =
       end
   in get_moves [] pieces
 
-let evaluate t =
-  failwith "Unimplemented"
+(** [evaluate_state t] is the evaluation function for a state [t].
+    Currently this evaluation function simply returns the difference in the
+    number of pieces each player has. *)
+let evaluate_state t =
+  let active_color = Some (Checkers.get_player t) in
+
+  let active_pieces = 
+    t |> Checkers.get_board 
+    |> List.filter (fun sq -> sq = active_color)
+    |> List.length
+  in
+
+  let opposing_pieces = 
+    t |> Checkers.get_board 
+    |> List.filter (fun sq -> sq = active_color)
+    |> List.length
+  in
+
+  active_pieces - opposing_pieces
+
+let evaluate_move t m =
+  Checkers.move t (fst m) (snd m) |> evaluate_state
+
+(** [next_states t] is all the next possible valid states from [t]. *)
+let next_states t = 
+  valid_moves t |> List.map (fun (pos1, pos2) -> Checkers.move t pos1 pos2)
+
+let maximize states : Checkers.state =
+  states |> List.map (fun st -> st, evaluate_state st) 
+  |> List.fold_left (fun (max_state, max_val) (st, v) -> 
+      if max_val > v then (max_state, max_val) else (st, v)) 
+    (List.nth states 1, min_int)
+  |> fst
+
+let minimize states : Checkers.state = 
+  states |> List.map (fun st -> st, evaluate_state st) 
+  |> List.fold_left (fun (min_state, min_val) (st, v) -> 
+      if min_val < v then (min_state, min_val) else (st, v)) 
+    (List.nth states 1, max_int)
+  |> fst
+
+let rec minimax acc t depth =
+  (* get all next possible states *)
+  t |> next_states 
+  (* map next states to resulting minimized state after opposing player plays *)
+  |> List.map (fun t -> minimize (next_states t))
 
 let next_move t =
-  failwith "Unimplemented"
+  let best_state = t |> next_states |> maximize in
+
+  let rec get_move = function
+    | [] -> failwith "impossible"
+    | (a,b)::y -> 
+      begin
+        if Checkers.move t a b = best_state then (a,b) else
+          get_move y
+      end
+  in t |> valid_moves |> get_move
--- checkersAI.mli
+++ checkersAI.mli
@@ -3,15 +3,33 @@ open Checkers
 type move = Checkers.position * Checkers.position
 
 (** [piece_moves t pos] is a list of positions that a checker piece located at
-    position [pos] can move to, regardless of who the active player is. *)
+    position [pos] can move to, regardless of who the active player is.
+    Requires: [pos] is a valid position. *)
 val piece_moves : Checkers.state -> Checkers.position -> Checkers.position list
 
 (** [valid_moves t] is all the possile moves given state [t]. *)
 val valid_moves : Checkers.state -> move list
 
-(** [evaluate t] quantifies how desirable the state [t] is for the active
-    player. *)
-val evaluate : Checkers.state -> int
+(** [evaluate_state t] quantifies how desirable the state [t] is for the 
+    active player. *)
+val evaluate_state : Checkers.state -> int
+
+(** [evaluate_move t m] quantifies how desirable the state [t] is for the 
+    active player if they make the move [m] using [evaluate_state]. *)
+val evaluate_move : Checkers.state -> move -> int
+
+(** [next_states t] is a list of all the next possible valid states from [t]. *)
+val next_states : Checkers.state -> Checkers.state list
+
+(** [maximize states] is the state with the maximal evaluation of [states]. *)
+val maximize : Checkers.state list -> Checkers.state
+
+(** [minimize states] is the state with the minimal evaluation of [states]. *)
+val minimize : Checkers.state list -> Checkers.state
+
+(** [minimax t depth] is the best move as calculated by [evaluate_move] using
+    the minimax algorithm with a search tree depth of [depth]. *)
+val minimax : Checkers.state -> int -> move
 
 (** [next_move t] is the move that the AI determines to take if it is the
     active player in state [t]. *)
